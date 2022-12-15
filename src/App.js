7d1b5969af6568deed2eb399cddeadbd91e4a2a0
benjiemalagayo <malagayo.benjie20@gmail.com>
2022-12-16 00:26:53 +0800

    used hooks (useParams, useNavigate) and Fetch data from the API

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Header from './components/Header';
 import Employees from './pages/Employees';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Customers from './pages/Customers';
+import Dictionaries from './pages/Dictionaries';
+import Definition from './pages/Definition';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -15,7 +18,14 @@ function App() {
       <Header>
         <Routes>
           <Route path='/employees' element={<Employees/>} />
+          <Route path='/dictionaries' element={<Dictionaries/>} />
+          <Route 
+            path='/definition/:search' 
+            element={<Definition/>} 
+          />
           <Route path='/customers' element={<Customers/>} />
+          <Route path='/404' element={<NotFound/>} />
+          <Route path='*' element={<NotFound/>} />
         </Routes>
       </Header>
     </BrowserRouter>
